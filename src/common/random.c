840e80503932cbc0610cb1f2f73b70a5950bfee0
Joey Herguth <oitzujoey@gmail.com>
2025-02-05 18:10:41 -0800

    Add `random` function to Lua.

---
+++ src/common/random.c
@@ -0,0 +1,6 @@
+#include <time.h>
+#include <stdlib.h>
+
+void random_init(void) {
+	srand(time(0));
+}
---
+++ src/common/random.h
@@ -0,0 +1,6 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+void random_init(void);
+
+#endif // RANDOM_H
